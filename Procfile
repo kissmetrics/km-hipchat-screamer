f19859586bf76689259b2802d52cfcc143b181fc
Ernest W. Durbin III <edurbin@kissmetrics.com>
2013-09-03 20:28:52 -0400

    create basic Flask app running under gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn km-hipchat-screamer:app.app
---
+++ km-hipchat-screamer/app.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+km-hipchat-screamer.app
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Module providing the application routes for the KISSmetrics HipChat Webhook service
+"""
+
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+#-------
+# Routes
+#-------
+
+@app.route('/')
+def root_route():
+    """Generate root response"""
+
+
+    body = { "Title": "KISSmetrics HipChat Webhook endpoint" }
+
+    return jsonify(body)
